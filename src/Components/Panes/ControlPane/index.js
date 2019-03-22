bbc9a2d8161ea685fd8e66346ddb0b68ea4481a4
Michael Royster <github@michaelroyster.org>
2019-03-21 22:05:33 -0400

    Removed control shape from index

--- src/Components/Panes/ControlPane/index.js
+++ src/Components/Panes/ControlPane/index.js
@@ -2,4 +2,3 @@ import ControlPane from './ControlPane';
 
 export default ControlPane;
 export {default as ControlButton} from './ControlButton';
-export {default as ControlSahpe} from './ControlShape';
